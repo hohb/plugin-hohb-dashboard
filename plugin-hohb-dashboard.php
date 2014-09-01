a35a5075e6209762d149d9e6db2c2d5545d9d595
gertjan <gertjan@homeofhappybrands.nl>
2014-09-01 11:29:42 +0200

    Initial commit

---
+++ plugin-hohb-dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: HoHB Dashboard
+Plugin URI: http://www.homeofhappybrands.nl
+Description: Home of Happy Brands Dashboard Theme
+Author: Home of Happy Brands
+Version: 1.0
+Author URI: http://www.homeofhappybrands.nl
+*/
+
+?>
